63a2083d0b0932a41de332f3ddb9deb66dd51519
ggreen <gregoryg@vmware.com>
2023-09-01 22:00:08 -0400

    Adding reporting

---
+++ applications/rabbit-vs-kafka-report-app/src/main/java/experiments/streaming/report/ReportApp.java
@@ -0,0 +1,11 @@
+package experiments.streaming.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportApp {
+    public static void main(String[] args) {
+        SpringApplication.exit(SpringApplication.run(ReportApp.class, args));
+    }
+}
